0ab9c84b918d66c2769d39acf07846c6b840dca7
Ryan Bryce <ryan.bryce1992@gmail.com>
2017-07-13 00:03:19 -0600

    finished the this keyword. learned that the value of t'this' is assined only when a method calls it. if a regular function calls trys to access 'this', the rules set it to the default context, which is the window obj. these rules still apply even when 'this' is called by a function inside a method.

--- script.js
+++ script.js
@@ -99,33 +99,79 @@
 // Example to show the differece between execution stack and scope chain
 
 
-var a = 'Hello!';
-first();
+// var a = 'Hello!';
+// first();
+//
+// function first() {
+//     var b = 'Hi!';
+//     second();
+//
+//     function second() {
+//         var c = 'Hey!';
+//         //this call works because of lexical scopeing. it has acess to the third function
+//         //because of lexical scopeing on the scope chain.
+//         third()
+//     }
+// }
 
-function first() {
-    var b = 'Hi!';
-    second();
-
-    function second() {
-        var c = 'Hey!';
-        //this call works because of lexical scopeing. it has acess to the third function
-        //because of lexical scopeing on the scope chain.
-        third()
-    }
-}
-
-function third() {
-    var d = 'John';
-    //the console log below wont work because of lexical scoping, its still in the window context obj.
-    //beecause of scope it can not go into a function and access the function's variables. because they or stored in
-    //a different context obj which has its own scope. however, we could return both variables and access them that way.
-    // console.log(a + b + c + d);
-    console.log(a, d);
-
-}
+// function third() {
+//     var d = 'John';
+//     //the console log below wont work because of lexical scoping, its still in the window context obj.
+//     //beecause of scope it can not go into a function and access the function's variables. because they or stored in
+//     //a different context obj which has its own scope. however, we could return both variables and access them that way.
+//     // console.log(a + b + c + d);
+//     console.log(a, d);
+//
+// }
 
 
 
 
 ///////////////////////////////////////
 // Lecture: The this keyword
+//points to the golbal window object
+// console.log(this);
+// calculateAge(1984)
+// function calculateAge(year) {
+//   console.log(2017 - year);
+//   //still points at the window object because it's technically a method that is still on the window object
+//   // because this VO is where the method was defined. and it would be the same as calling window.calculateAge()
+//   console.log(this);
+// }
+
+
+var john = {
+  name: 'john',
+  yearOfBirth: '1984',
+  calculateAge: function() {
+    //this allways refers to the obj in which the method is called in.
+    //so since the method is called on teh john obj, the this keywa
+    console.log(this);
+    //"this" is still the jhon obj
+    console.log(2017 - this.yearOfBirth);
+    // inner()
+    // //now this is strange, but not really. Thats because the rule is that when a regular function call happens,
+    // //the default obj is the window obj. this is not a method. and even though its inside a method its still a reguar function.
+    // //and this means that when we call it, it will point to the john obj but it will defaut to the window obj.
+    // function inner() {
+    //   console.log(this);
+    // }
+  }
+}
+
+john.calculateAge()
+
+var mike = {
+  name: 'Mike',
+  yearOfBirth: '1956'
+}
+//we are borrowing the john method and assigning it to the mike obj.
+mike.calculateAge = john.calculateAge;
+//when we noe call calculateAge, it is being called as a method on the mike obj. and now 'this' refers to the mike obj.
+mike.calculateAge()
+console.log('wow');
+//me messing with call and bind methods, to change the context of the this to be explicitly bound to the mike obj.
+john.calculateAge.call(mike);
+console.log('doge');
+var mikeCalc = john.calculateAge.bind(mike);
+mikeCalc();
